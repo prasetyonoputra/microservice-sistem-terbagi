54656b37f989567ea38e7e94f828b7659446d03c
yossandiimran <yossandiimran1307@gmail.com>
2023-02-28 22:58:44 +0700

    List Order - Add Order

--- cart/main.go
+++ cart/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -12,44 +14,43 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type User struct {
-	id        int       `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
-type UserResponse struct {
-	id        int    `json:"id"`
-	Name      string `json:"name"`
-	Email     string `json:"email"`
-	CreatedAt string `json:"created_at"`
-}
-
 type Cart struct {
-	id          int       `json:"id"`
-	idBarang    int       `json:"idBarang"`
-	idUser      string    `json:"idUser"`
-	CreatedAt   time.Time `json:"createdAt"`
-	qtyBarang   int       `json:"qtyBarang"`
-	hargaSatuan int       `json:"hargaSatuan"`
+	Id          int       `json:"id"`
+	IdUser      string    `json:"id_user"`
+	IdBarang    string    `json:"id_barang"`
+	QtyBarang   int       `json:"qty_barang"`
+	HargaSatuan int       `json:"harga_satuan"`
+	CreatedAt   time.Time `json:"created_at"`
+	TotalHarga  int       `json:"total_harga"`
+	Status      string    `json:"status"`
 }
 
 type CartResponse struct {
-	id          int    `json:"id"`
-	idBarang    int    `json:"idBarang"`
-	idUser      string `json:"idUser"`
-	CreatedAt   string `json:"createdAt"`
-	qtyBarang   int    `json:"qtyBarang"`
-	hargaSatuan int    `json:"hargaSatuan"`
+	Id          int    `json:"id"`
+	IdUser      string `json:"id_user"`
+	IdBarang    string `json:"id_barang"`
+	QtyBarang   int    `json:"qty_barang"`
+	HargaSatuan int    `json:"harga_satuan"`
+	CreatedAt   string `json:"created_at"`
+	TotalHarga  int    `json:"total_harga"`
+	Status      string `json:"status"`
+}
+
+type ResponseData struct {
+	IsAuth bool   `json:"isAuth"`
+	Id     string `json:"id"`
+}
+
+type ErrorResp struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
 }
 
 func main() {
-	// Set connection parameters
 	host := "localhost"
 	port := "5432"
 	user := "postgres"
-	password := "password"
+	password := "admin"
 	dbname := "microservice"
 
 	// Open a connection to the database
@@ -63,63 +64,42 @@ func main() {
 	r := mux.NewRouter()
 
 	// Define routes
-	r.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		getUsers(w, r, db)
-	}).Methods("GET")
+	r.HandleFunc("/listOrder", func(w http.ResponseWriter, r *http.Request) {
+		getOrder(w, r, db)
+	}).Methods("POST")
 
-	r.HandleFunc("/cart", func(w http.ResponseWriter, r *http.Request) {
-		getCart(w, r, db)
-	}).Methods("GET")
+	r.HandleFunc("/addOrder", func(w http.ResponseWriter, r *http.Request) {
+		addOrder(w, r, db)
+	}).Methods("POST")
 
 	// Start the server
 	log.Println("Server started on port 8881")
 	log.Fatal(http.ListenAndServe(":8881", r))
 }
 
-func getUsers(w http.ResponseWriter, r *http.Request, db *sql.DB) {
-	// Query the database
-	rows, err := db.Query("SELECT id, name, email, created_at FROM users")
+func getOrder(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
+	// Get Body Form Method
+	type Body struct {
+		TokenAuth string `json:"token_auth"`
+	}
+
+	var body Body
+
+	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	// Loop through the rows and scan the results
-	var users []User
-	for rows.Next() {
-		var user User
-		err := rows.Scan(&user.id, &user.Name, &user.Email, &user.CreatedAt)
-		if err != nil {
-			log.Fatal(err)
-		}
-		users = append(users, user)
-	}
-	if err := rows.Err(); err != nil {
-		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
-	// Convert time.Time to string in desired format
-	var userResponses []UserResponse
-	for _, user := range users {
-		userResponses = append(userResponses, UserResponse{
-			id:        user.id,
-			Name:      user.Name,
-			Email:     user.Email,
-			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
-		})
-	}
+	auth := getAuth(body.TokenAuth)
+	log.Println(auth.IsAuth)
 
-	// Encode the response as JSON and write to response writer
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(userResponses)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+	userId := auth.Id
 
-func getCart(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	// Query the database
-	rows, err := db.Query("SELECT * FROM cart")
+	sql := "SELECT * FROM cart WHERE id_user = '" + userId + "'"
+	rows, err := db.Query(sql)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -129,12 +109,13 @@ func getCart(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	var carts []Cart
 	for rows.Next() {
 		var cart Cart
-		err := rows.Scan(&cart.id,
-			&cart.idBarang,
-			&cart.idUser,
+		err := rows.Scan(&cart.Id,
+			&cart.IdUser,
+			&cart.IdBarang,
+			&cart.QtyBarang,
+			&cart.HargaSatuan,
 			&cart.CreatedAt,
-			&cart.qtyBarang,
-			&cart.hargaSatuan)
+			&cart.TotalHarga, &cart.Status)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -149,12 +130,14 @@ func getCart(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	var cartResponse []CartResponse
 	for _, cart := range carts {
 		cartResponse = append(cartResponse, CartResponse{
-			id:          cart.id,
-			idBarang:    cart.idBarang,
-			idUser:      cart.idUser,
+			Id:          cart.Id,
+			IdUser:      cart.IdUser,
+			IdBarang:    cart.IdBarang,
+			QtyBarang:   cart.QtyBarang,
+			HargaSatuan: cart.HargaSatuan,
 			CreatedAt:   cart.CreatedAt.Format("2006-01-02 15:04:05"),
-			qtyBarang:   cart.qtyBarang,
-			hargaSatuan: cart.hargaSatuan,
+			TotalHarga:  cart.TotalHarga,
+			Status:      cart.Status,
 		})
 	}
 
@@ -164,4 +147,96 @@ func getCart(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func addOrder(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	type Body struct {
+		TokenAuth   string `json:"token_auth"`
+		IdBarang    string `json:"id_barang"`
+		QtyBarang   int    `json:"qty_barang"`
+		HargaSatuan int    `json:"harga_satuan"`
+	}
+
+	var body Body
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	hargaTotal := body.HargaSatuan * body.QtyBarang
+
+	auth := getAuth(body.TokenAuth)
+	checkAuth := auth.IsAuth
+	if checkAuth {
+		cart := Cart{
+			IdUser:      auth.Id,
+			IdBarang:    body.IdBarang,
+			QtyBarang:   body.QtyBarang,
+			HargaSatuan: body.HargaSatuan,
+			CreatedAt:   time.Now(),
+			TotalHarga:  hargaTotal,
+			Status:      "order",
+		}
+		sqlStatement := `INSERT INTO cart (id_user, id_barang, qty_barang, harga_satuan, created_at, total_harga, status) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
+		var id int
+		err = db.QueryRow(sqlStatement, cart.IdUser, cart.IdBarang, cart.QtyBarang, cart.HargaSatuan, cart.CreatedAt, cart.TotalHarga, cart.Status).Scan(&id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(cart)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		errorResp := ErrorResp{
+			Error:   true,
+			Message: "Terjadi Kesalahan",
+		}
+		err = json.NewEncoder(w).Encode(errorResp)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func getAuth(t string) ResponseData {
+	url := "http://localhost:3000/api/auth-service/profile"
+
+	data := map[string]string{
+		"token": t,
+	}
+	payload, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer(payload))
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var responseData ResponseData
+	err = json.Unmarshal(body, &responseData)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return responseData
 }
